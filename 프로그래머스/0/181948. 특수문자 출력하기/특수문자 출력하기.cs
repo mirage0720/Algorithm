e989b22e3a70451081a644835d5b7b506a263e50
mirage0720 <127704600+mirage0720@users.noreply.github.com>
2024-06-18 12:29:43 +0900

    [level 0] Title: 특수문자 출력하기, Time: 15.78 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string specialCharacters = "!@#$%^&*(\\'\"<>?:;";
+        Console.WriteLine(specialCharacters);
+    }
+}
